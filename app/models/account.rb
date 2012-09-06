d9fd349f47a9cc3cbbb945c3e185a30dc7b4a922
Sören Link (Macbook) <linksoeren@googlemail.com>
2012-09-06 23:16:30 +0200

    Devise integration. For real now
    Seriously

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,42 +1,13 @@
-# The account Class. Holds all information for a user account including authentication
-# information (name, password, email address etc).
+# The account Class. Holds all information for a user account including
+# authentication information (name, password, email address etc).
 #
-# One Account can have many characters which need to be valid EVE Online characters. (see Character)
-# An Account also has only one main_character
+# One Account can have many characters which need to be valid EVE Online
+# characters. (see Character) An Account also has only one main_character
 class Account < ActiveRecord::Base
   attr_accessor :main_character_name
-  # Hash password
-  attr_accessible :name, :email, :email_confirmation, :password, :password_confirmation
-
-  has_secure_password
-
-  validates_uniqueness_of   :name
-  validates_format_of       :name, with: /^\w[\w\d ]+$/
-  validates_length_of       :name, within: 3..24
-
-  validates_presence_of     :password, on: :create
-  validates_length_of       :password, minimum: 5, on: :create
-
-  validates_uniqueness_of   :email
-  validates_format_of       :email, with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-  validates_confirmation_of :email
+  devise :database_authenticatable, :confirmable, :recoverable, :registerable,
+    :rememberable, :trackable, :vaidateable, :lockable
 
   has_many :characters, :dependent => :destroy, :include => :roles
   belongs_to :main_character, :class_name => "Character"
-
-  # Authenticate user with name and password
-  def self.authenticate(name, password)
-    account = find_by_name(name)
-    return account if account and account.authenticate password
-  end
-
-  private
-  def generate_token(columns)
-    columns = [] << columns unless columns.is_a?(Array)
-    columns.each do |column|
-      begin
-        self[column] = SecureRandom.urlsafe_base64
-      end while Account.exists?(column => self[column])
-    end
-  end
 end
--- db/migrate/20110206203716_create_accounts.rb
+++ db/migrate/20110206203716_create_accounts.rb
@@ -1,18 +1,15 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
-      t.string :name
       t.integer :main_character_id
       t.database_authenticatable
       t.confirmable
       t.recoverable
       t.rememberable
       t.trackable
-      t.token_authenticatable
 
       t.timestamps
     end
-    add_index :accounts, :name
     add_index :accounts, :main_character_id
   end
 end
